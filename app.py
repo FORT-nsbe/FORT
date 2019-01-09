6712787ee771af524fac6972c52fe2eb124accf5
Xavier Kelly <xavierk98@yahoo.com>
2019-01-08 20:29:51 -0500

    remove all unneccesary code from old repo

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, request
 from dotenv import load_dotenv
 import bot
 import os
-import imgur
 import requests
 
 
@@ -17,38 +16,12 @@ def receive_message():
         return "I'm a simple GroupMe bot built with Python, I take and save pictures shared in a group chat"
     else: # issue response to the received post request (message)
         response = request.get_json()
+        if(response['text'].startswitch('Fort Alert')):
+            print('Awesome')
         print(response['text'])
-        # if the message received is just text with no image
-        if(response['attachments'] == []):
-            pass
-        else: # the message is an image so post it
-             if(response['attachments'][0]['type'] == 'image'):
-                print(response['attachments'][0]['url'])
-            #     img_url = response['attachments'][0]['url']
-            #     filename = img_url.split('/')[-1]
-            #     r = requests.get(img_url, stream=True)
-            #     if r.status_code == 200:
-            #         content_type = r.headers['content-type']
-            #         file_type = content_type.split('/')[-1]
-            #         print(content_type)  
-            #         with open('./imgs/' + filename + '.' + file_type, 'wb') as f:
-            #             for chunk in r:
-            #                 f.write(chunk) 
-            #             print('Image: ' + filename + ' saved')               
+  
         return 'good'    
 
 if __name__ == '__main__':
-    this_dir = os.getcwd()
-    img_file = this_dir + '/imgs'
-    bot_inst = bot.Bot(os.environ['bot_id'], os.environ['token'], os.environ['group_ID'])
-
-    if not os.path.isdir(img_file):
-        print('Creating image folder at: ' + img_file)
-        os.mkdir(img_file)
-        print('Populating image folder at: ' + img_file + ' this may take a moment')
-        bot_inst.run()
-        print('')
-        print('Finished')
-
-    print('Listening for new images')
+    print('Listening..')
     app.run()
--- bot.py
+++ bot.py
@@ -33,69 +33,11 @@ class Bot:
         #     #for chunk in r_file.iter_content(chunk_size=128):
         #         fd.write(content)
 
-    def __push_img(img):
-        req = requests.post('https://7bfa877e.ngrok.io', data = img)
-        if(req.status_code == 200):
-            print('success')
-
     def postMessage(self, text):
         url = '/bots/post'
         params = {'bot_id': self.bot_token, 'text': text}
         return self.__makePostRequest(url, params)
 
-    # Returns an array of all images shared in a group
-
-    def __get_images(self):
-        base_url = 'https://api.groupme.com/v3'
-        url = f'/groups/{self.group_id}/messages'
-        params = {'token': self.auth_token} # before_id, since_id, after_id
-        messagesResponse = requests.get(base_url + url, params = params).json()
-        msg_count = messagesResponse['response']['count']
-        img_list = []
-        i = 0
-        x = 0
-        while i < msg_count:
-            if(x < 20):
-                if(messagesResponse['response']['messages'][x]['attachments'] == []):
-                    pass
-                else:
-                    if(messagesResponse['response']['messages'][x]['attachments'][0]['type'] == 'image'): # message should be an image at this point
-                        img_url = messagesResponse['response']['messages'][x]['attachments'][0]['url']
-                        img_list.append(img_url)
-                        print('Image ' + str(i) + '/' + str(msg_count) + ' collected', end='\r')
-                if(x == 19):
-                    id = messagesResponse['response']['messages'][x]['id']
-                x += 1
-            else:
-                params = {'token': self.auth_token, 'before_id': id} # before_id, since_id, after_id
-                messagesResponse = requests.get(base_url + url, params = params).json()
-                x = 0
-            i += 1
-        return img_list
-
-    def post_all_images(self):
-            img_list = self.get_images()
-            imgur.post_images(img_list) # posts images to the Imgur account with an array
-
-    def __post_images_FS(self):
-        img_list = self.__get_images()
-
-        for img_url in img_list:
-            filename = img_url.split('/')[-1]
-            r = requests.get(img_url, stream=True)
-            if r.status_code == 200:
-                content_type = r.headers['content-type']
-                file_type = content_type.split('/')[-1]
-                with open('./imgs/' + filename + '.' + file_type, 'wb') as f:
-                    for chunk in r:
-                        f.write(chunk)
-                    print('Image: ' + filename + ' saved', end='\r')
-        return img_list
-
-
-    def imageExists(url): # should only be run to check if an image has been shared already
-        pass
-
-    def run(self):
-        img_list = self.__post_images_FS()
-        return img_list
+    # def run(self):
+    #     img_list = self.__post_images_FS()
+    #     return img_list
